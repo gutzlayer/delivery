aa91e34b0dfb1aefdeea092ee12b3929f7d1b709
gutzlayer <gustavocarmo56@gmail.com>
2021-09-22 17:38:30 -0300

    feat: Create a project and your node connection

---
+++ src/components/TrackingEvents/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+
+function TrackingEvents({ events }) {
+    if (!events || events.length ===0) return null;
+    return (
+        <Fragment>
+            <h1>Tracking Events</h1>
+            <ul className="list-group">
+                {events.map(item => {
+                    return (
+                        <li key={item.descricao} className="list-group-item">
+                            <span>{item.data}</span>
+                            <span>{item.descricao}</span>
+                            <span>{item.cidade}/{item.uf}</span>
+                        </li>)
+                })}
+            </ul>
+        </Fragment>
+    );
+}
+
+export default TrackingEvents;
